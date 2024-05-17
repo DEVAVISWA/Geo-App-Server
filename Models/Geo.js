490cc3ecea5367f7808a2ee575a51adf14820179
DEVAVISWA <devaviswa15@gmail.com>
2024-05-17 12:54:43 +0530

    geo json schema

---
+++ Models/Geo.js
@@ -0,0 +1,21 @@
+const { default: mongoose } = require("mongoose");
+
+const locationSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    default: "Point",
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere", //2d will not take its cicumferance of the index will be 2dSphere
+  },
+});
+
+const GeoSchema = new mongoose.Schema({
+  //insted of multiple nesting i saving it in a seperate variable and giving its value to the geometry
+  geometry: locationSchema,
+});
+
+const Geo = mongoose.model("Geo", GeoSchema);
+
+module.exports = Geo;
